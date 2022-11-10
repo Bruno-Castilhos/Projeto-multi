b2f798551d8dceb308e34dba98af334124dc7e59
Bruno-Castilhos <brunosilvacastilhos@gmail.com>
2022-11-10 20:23:34 -0300

    ajuste teste

--- src/components/Modals/Modal1.tsx
+++ src/components/Modals/Modal1.tsx
@@ -8,7 +8,7 @@ export function Modal1({id = 'Modal', onClose = () => {}}) {
 
   return (
     <div id={id} onClick={handleOutsideClick} className="fixed inset-0 backdrop-blur flex justify-center items-center ">
-      <div className=" bg-zinc-800 w-96 h-96 flex flex-col gap-12 rounded-3xl shadow-lg shadow-brand">
+      <div className=" bg-zinc-800 w-[784px] h-[584px] flex flex-col gap-12 rounded-3xl shadow-lg shadow-brand">
         <header className="flex justify-between items-center px-8 py-6">
           <div>Relatório de Pizza</div>
           <button onClick={onClose}>X</button>
